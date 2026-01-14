5ff4eb8c1d748fa2b4b496c0dfc72d2e3b06433c
mahdi <hatamimahdi32@gmail.com>
2026-01-14 10:25:31 -0800

    Degree of heat commit

---
+++ convert_fahranheit.py
@@ -0,0 +1,6 @@
+# Task: converting celesius to fahrenheit
+
+celsius = float(input("Enter a degree in celsius:"))
+# convert to fahrenheit
+fahrenheit = round((9/5)* celsius + 32,3)
+print(celsius,"degree of celsius is",fahrenheit,"fahrenheit")
