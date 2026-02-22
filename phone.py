53098662db10abbb035601461fe5d84fdd7646d7
Mahdi <hatamimahdi32@gmail.com>
2026-02-22 16:05:20 +0430

    using string silicing

---
+++ phone.py
@@ -0,0 +1,24 @@
+def main():
+
+    phone = '+93 792310388'
+    reverse_phone(phone)
+
+def phone_(phone):
+    return print(phone[0:5])   # +93 7
+
+
+def get_start_phone(phone):
+    return print(phone[ : 5])   # +93 7
+
+def mdiddle_phone(phone):
+    return print(phone[2:8])    # 3 7923
+
+def end_phone(phone):
+    return print(phone[8: ])    #  10388
+
+
+def reverse_phone(phone):
+    return print(phone[-4: ])  # 0388
+
+
+main()
